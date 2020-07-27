6c38b68ae4257d9f1f215610f81ff40775cda5b1
Pony <ponyma2100@gmail.com>
2020-07-27 21:08:35 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() //取出Todo Model的所有資料
     .lean() //把Mongoose的Model物件轉換成乾淨的JavaScript資料陣列
+    .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos })) //將資料傳給index樣板
     .catch(error => console.error(error))
 })
