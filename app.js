635e2a3568f92e8716a02a34be43a60c9c6f0985
Pony <ponyma2100@gmail.com>
2020-08-19 02:08:52 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const bodyParser = require('body-parser') // 引用body-parser
 const methodOverride = require('method-override')
 
@@ -14,6 +15,12 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 // hbs元件掛載到我們的主程式裡，開始啟用
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
