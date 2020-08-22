5b00e8a782df01969276328b3bbaffdd6ead9e87
Pony <ponyma2100@gmail.com>
2020-08-22 18:38:06 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
@@ -6,16 +7,19 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
   // 設定本地登入策略
+  //第一個參數是使用者的輸入值，而第二個參數是資料庫裡的雜湊值，brcypt 會幫我們做比對，並回傳布林值，在文中我們用 isMatch 來代表
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
           return done(null, false, { message: 'This email is not registered!' })
         }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect.' })
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, { message: 'Email or Password incorrect.' })
+          }
+          return done(null, user)
+        })
       })
       .catch(err => done(err, false))
   }))
